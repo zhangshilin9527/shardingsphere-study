10a9b39d2a7b4ae80d45cf783eb89821481da740
zhang <zhangsl312@chinaunicom.cn>
2021-01-25 10:59:49 +0800

    first commit

---
+++ src/main/java/com/xiaolinzi/shardingsphere/study/ShardingsphereStudyApplication.java
@@ -0,0 +1,15 @@
+package com.xiaolinzi.shardingsphere.study;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan(basePackages = "com.xiaolinzi.shardingspere.study.mapper")
+@SpringBootApplication
+public class ShardingsphereStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingsphereStudyApplication.class, args);
+    }
+
+}
